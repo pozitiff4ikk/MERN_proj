febd3436f37a837ce3058168cfae05e10ef248f8
pozitiff4ikk <sluchuk99@gmail.com>
2021-06-03 15:20:21 +0300

    connecting to db

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const mongoose = require('mongoose')
 const exprsBars = require('express-handlebars');
 const path = require('path');
 
+const PORT = process.env.PORT || 3000
+
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }))
 
 express.static(path.join(__dirname, 'views'));
 
@@ -18,11 +20,27 @@ app.engine('.hbs', exprsBars({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
+async function start() {
+    try {
+        await mongoose.connect(
+            'mongodb+srv://vlad:1q2w3e4r@cluster0.zheix.mongodb.net/users?authSource=admin',
+            {
+                useNewUrlParser: true,
+                useFindAndModify: false,
+                useUnifiedTopology: true
+            }
+        )
+        app.listen(PORT, (err) => {
+            if(err){
+                console.log(err);
+            } else {
+                console.log('Listening port', PORT)
+            }
 
-app.listen(5000, (err) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log('Listening 5000...');
+        })
+    } catch (e) {
+        console.log(e)
     }
-})
+}
+
+start();
